cbc3e08a3f992cc98aabcbf6233a6a63f7b8daf1
Nat Bennett <natalie.j.bennett@gmail.com>
2022-06-24 13:29:48 -0700

    Tests for the OPTION command, currently against public demo/test server

--- mix.exs
+++ mix.exs
@@ -20,10 +20,9 @@ defmodule S83Server.MixProject do
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  def deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.8"}
     ]
   end
 end
---
+++ test/spec_test.exs
@@ -0,0 +1,20 @@
+defmodule SpecTest do
+  use ExUnit.Case, async: true
+
+  test "server responds to OPTIONS" do
+    response = HTTPoison.options!("https://bogbody.biz/")
+    
+    body = response.body()
+    status = response.status_code()
+    assert body == ""
+    assert status == 204
+  end
+  
+  test "OPTIONS returns allowed methods" do
+    response = HTTPoison.options!("https://bogbody.biz/")
+    headers = response.headers()
+    
+    methods = List.keyfind(headers, "Access-Control-Allow-Methods", 0)
+    assert methods == "GET, PUT, OPTIONS"
+  end
+end
