b18d71a1f42a187a6c549b8b7faa2f94d955d4b4
Nat Bennett <natalie.j.bennett@gmail.com>
2022-06-29 19:00:24 -0700

    Begin basic GET endpoint

---
+++ lib/elderflower_web/controllers/board_controller.ex
@@ -0,0 +1,7 @@
+defmodule ElderflowerWeb.BoardController do
+  use ElderflowerWeb, :controller
+
+  def show(conn, _params) do
+    text(conn, "")
+  end
+end
--- test/spec_test.exs
+++ test/spec_test.exs
@@ -1,20 +1,12 @@
 defmodule SpecTest do
   use ExUnit.Case, async: true
 
-  test "server responds to OPTIONS" do
-    response = HTTPoison.options!("https://bogbody.biz/")
-    
+  test "server accepts requests to GET a board" do
+    response = HTTPoison.get!("http://localhost:4000/ca93846ae61903a862d44727c16fed4b80c0522cab5e5b8b54763068b83e0623")
+ 
     body = response.body()
     status = response.status_code()
     assert body == ""
-    assert status == 204
-  end
-  
-  test "OPTIONS returns allowed methods" do
-    response = HTTPoison.options!("https://bogbody.biz/")
-    headers = response.headers()
-    
-    { _, methods } = List.keyfind(headers, "Access-Control-Allow-Methods", 0)
-    assert methods == "DELETE, GET, OPTIONS, PUT"
+    assert status == 200
   end
 end
