eaa59c36d6aaef8786c31921287f9953af76fd9a
Nat Bennett <natalie.j.bennett@gmail.com>
2022-07-10 19:11:41 -0700

    Add scaffold 'get' and 'put' board commands

---
+++ lib/elderflower/server.ex
@@ -0,0 +1,10 @@
+defmodule Elderflower.Server do
+
+   def update_board(post) do
+     { :ok, post }
+   end
+
+   def get_board(public_key) do
+     "some board contents"
+   end
+end
---
+++ test/elderflower_web/controllers/board_controller_test.exs
@@ -0,0 +1,26 @@
+defmodule ElderflowerWeb.BoardControllerTest do
+  use ElderflowerWeb.ConnCase
+
+  alias Elderflower.Server
+
+  @create_attrs %{body: "some board contents", "public_key": "some board key" }
+
+  def fixture(:post) do
+    {:ok, post} = Server.update_board(@create_attrs)
+    post
+  end
+
+  describe "show" do
+    test "returns a board", %{conn: conn} do
+      board = fixture(:post)
+      conn = get(conn, Routes.board_path(conn, :show, "lupus"))
+      assert html_response(conn, 200) == board.body
+    end
+  end
+
+  describe "update" do
+    test "accepts requests to update a board", %{conn: conn} do
+      put(conn, Routes.board_path(conn, :update, "lupus"))
+    end
+  end
+end
