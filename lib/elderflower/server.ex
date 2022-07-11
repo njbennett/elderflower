34fd7f7267f52a01ea5e13143f442b80ad2fdee7
Nat Bennett <natalie.j.bennett@gmail.com>
2022-07-10 20:56:06 -0700

    Create a Server context for managing boards

--- lib/elderflower/server.ex
+++ lib/elderflower/server.ex
@@ -1,5 +1,7 @@
 defmodule Elderflower.Server do
 
+   alias Elderflower.Repo
+
    def update_board(post) do
      { :ok, post }
    end
@@ -7,4 +9,100 @@ defmodule Elderflower.Server do
    def get_board(_public_key) do
      "some board contents"
    end
+
+  alias Elderflower.Server.Board
+
+  @doc """
+  Returns the list of boards.
+
+  ## Examples
+
+      iex> list_boards()
+      [%Board{}, ...]
+
+  """
+  def list_boards do
+    Repo.all(Board)
+  end
+
+  @doc """
+  Gets a single board.
+
+  Raises `Ecto.NoResultsError` if the Board does not exist.
+
+  ## Examples
+
+      iex> get_board!(123)
+      %Board{}
+
+      iex> get_board!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_board!(id), do: Repo.get!(Board, id)
+
+  @doc """
+  Creates a board.
+
+  ## Examples
+
+      iex> create_board(%{field: value})
+      {:ok, %Board{}}
+
+      iex> create_board(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_board(attrs \\ %{}) do
+    %Board{}
+    |> Board.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a board.
+
+  ## Examples
+
+      iex> update_board(board, %{field: new_value})
+      {:ok, %Board{}}
+
+      iex> update_board(board, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_board(%Board{} = board, attrs) do
+    board
+    |> Board.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a board.
+
+  ## Examples
+
+      iex> delete_board(board)
+      {:ok, %Board{}}
+
+      iex> delete_board(board)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_board(%Board{} = board) do
+    Repo.delete(board)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking board changes.
+
+  ## Examples
+
+      iex> change_board(board)
+      %Ecto.Changeset{data: %Board{}}
+
+  """
+  def change_board(%Board{} = board, attrs \\ %{}) do
+    Board.changeset(board, attrs)
+  end
 end
---
+++ test/elderflower/server_test.exs
@@ -0,0 +1,61 @@
+defmodule Elderflower.ServerTest do
+  use Elderflower.DataCase
+
+  alias Elderflower.Server
+
+  describe "boards" do
+    alias Elderflower.Server.Board
+
+    import Elderflower.ServerFixtures
+
+    @invalid_attrs %{content: nil, key: nil}
+
+    test "list_boards/0 returns all boards" do
+      board = board_fixture()
+      assert Server.list_boards() == [board]
+    end
+
+    test "get_board!/1 returns the board with given id" do
+      board = board_fixture()
+      assert Server.get_board!(board.id) == board
+    end
+
+    test "create_board/1 with valid data creates a board" do
+      valid_attrs = %{content: "some content", key: "some key"}
+
+      assert {:ok, %Board{} = board} = Server.create_board(valid_attrs)
+      assert board.content == "some content"
+      assert board.key == "some key"
+    end
+
+    test "create_board/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Server.create_board(@invalid_attrs)
+    end
+
+    test "update_board/2 with valid data updates the board" do
+      board = board_fixture()
+      update_attrs = %{content: "some updated content", key: "some updated key"}
+
+      assert {:ok, %Board{} = board} = Server.update_board(board, update_attrs)
+      assert board.content == "some updated content"
+      assert board.key == "some updated key"
+    end
+
+    test "update_board/2 with invalid data returns error changeset" do
+      board = board_fixture()
+      assert {:error, %Ecto.Changeset{}} = Server.update_board(board, @invalid_attrs)
+      assert board == Server.get_board!(board.id)
+    end
+
+    test "delete_board/1 deletes the board" do
+      board = board_fixture()
+      assert {:ok, %Board{}} = Server.delete_board(board)
+      assert_raise Ecto.NoResultsError, fn -> Server.get_board!(board.id) end
+    end
+
+    test "change_board/1 returns a board changeset" do
+      board = board_fixture()
+      assert %Ecto.Changeset{} = Server.change_board(board)
+    end
+  end
+end
